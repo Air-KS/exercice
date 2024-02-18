307ee80b968df0eea0c83ac25b1e5de69d3afe1b
Air-KS <kevinrogeret@gmail.com>
2024-02-18 15:48:40 +0100

    Exercice en C++

---
+++ C++/1-variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	// Commentaire
+	int age = 30;
+	double taille = 1.600;
+	std::string nom = "Zana";
+
+	// Affichage des variables
+	std::cout << "Je m'appelle " << nom << " !" << std::endl;
+	std::cout << "A l'heure acteuelle, j'ai : " << age << "ans" << std::endl;
+	std::cout << "Je suis petit de taille pour : " << taille << " mètres" << std:: endl;
+
+	return 0;
+}
